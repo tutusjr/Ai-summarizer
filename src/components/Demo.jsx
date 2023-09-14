5c8d4a550c9647b57b24993a64dbde4b9611cc43
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-09-14 16:12:23 +0300

    Ai is done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,8 +9,7 @@ const Demo = () => {
   });
 
   const [allArticles, setAllArticles] = useState([]);
-
-  
+  const [ copied , setCopied] = useState ("");
 
   const handleSubmit = async (e) => {
 
@@ -25,7 +24,7 @@ const Demo = () => {
       
       setArticle(newArticle);
       setAllArticles(updatedAllArticles)
-      console.log(newArticle)
+      
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
@@ -44,6 +43,11 @@ const Demo = () => {
     }
   }, [])
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  }
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* search */}
@@ -73,11 +77,15 @@ const Demo = () => {
             <div
               key={`link-${index}`}
               onClick={() => setArticle(item)}
-              className="link_card">
-                <div className="copy_btn">
-                  <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain"/>
+              className="link_card"
+            >
+                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                  <img 
+                    src={copied === item.url? tick : copy} 
+                    alt="copy_icon" 
+                    className="w-[40%] h-[40%] object-contain"/>
                 </div>
-                <p>
+                <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
                   {item.url}
                 </p>
             </div>
@@ -85,6 +93,31 @@ const Demo = () => {
         </div>
       </div>
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+            {isFetching ? (
+              <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+              ) : error ? (
+                <p className="font-inter font-bold text-black text-center">
+                  Something Went Wrong!
+                  <br />
+                  <span className="font-satoshi font-normal text-gray-700">
+                    {error?.data?.error}
+                  </span>
+                </p>
+              ) : (
+                article.summary && (
+                  <div className="flex flex-col gap-3">
+                    <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                      Article <span className="blue_gradient">Summary</span>
+                    </h2>
+                    <div className="summary_box">
+                      <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+                    </div>
+                  </div>
+                )
+              )
+            }
+      </div>
     </section>
   )
 }
